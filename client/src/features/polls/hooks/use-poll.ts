4fa271b38550268db3f71b99057ceac7ca4341e1
irish-mike <michaelgr@live.ie>
2026-06-08 18:46:01 +0200

    Add poll refetch and make owner_token NOT NULL

--- client/src/features/polls/hooks/use-poll.ts
+++ client/src/features/polls/hooks/use-poll.ts
@@ -4,6 +4,8 @@ import { getPoll } from "../api/api";
 import { poll_query_keys } from "../api/query-keys";
 import { getUserToken } from "../../../utils/browser";
 
+const poll_refetch_interval_ms = 5000;
+
 export function usePoll(id: number | null) {
     return useQuery({
         queryKey: poll_query_keys.detail(id),
@@ -15,5 +17,6 @@ export function usePoll(id: number | null) {
             return getPoll(id, getUserToken());
         },
         enabled: id !== null,
+        refetchInterval: poll_refetch_interval_ms,
     });
 }
